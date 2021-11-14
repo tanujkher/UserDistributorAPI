44fd22938f3bb2ce48f8e40afb6e0dfd4526fecf
tanujkher <tanujkher15@gmail.com>
2021-11-14 18:37:05 +0530

    Adding User API and access

--- AccessDistributorDetails.py
+++ AccessDistributorDetails.py
@@ -8,6 +8,79 @@ isAdmin = False
 def user_func():
     i = 'U'
     while(i == 'U'):
+        c = input(
+            'Enter 1 to view User Details(Used GET Request)(Accessible to all)\n' +
+            'Enter 2 to add User Details(Used POST Request)(Accessible to ADMIN ONLY)\n' +
+            'Enter 3 to edit User Details(Used PUT Request)(Accessible to ADMIN ONLY)\n' +
+            'Enter 4 to delete User Details(Used DELETE Request)(Accessible to ADMIN ONLY)\n'
+        )
+        if(c == '1'):
+            su = input('Enter user_ID to be searched\n OR\n* for all users\n')
+            if(su == '*'):
+                r = request.Request('http://127.0.0.1:5000/user', method='GET')  
+                try:
+                    res = request.urlopen(r)
+                    print(res.read())
+                except urllib.error.HTTPError as e:
+                    print(e.code, e.read())
+            else:  
+                r = request.Request('http://127.0.0.1:5000/user/{}'.format(su), method='GET')
+                try:
+                    res = request.urlopen(r)
+                    print(res.read())
+                except urllib.error.HTTPError as e:
+                    print(e.code, e.read())
+        if(c == '2'):
+            if(isAdmin):
+                au = input('Enter user_ID to be added\t')
+                r0 = request.Request('http://127.0.0.1:5000/user/{}'.format(au), method='GET')
+                try:
+                    r1 = request.urlopen(r0)
+                    print('User {} already exists'.format(au))
+                except urllib.error.HTTPError as e:
+                    print('Enter data of new user')
+                    aud = {'user_ID' : '{}'.format(au), 'password' : '{}'.format(input('Enter new user password\t')), 'role' : '{}'.format(input('Enter new user role\t')), 'isAdmin' : '{}'.format(input('Enter if new user is Admin or Not\t'))}
+                    data = urllib.parse.urlencode(aud)
+                    data = data.encode('ascii')
+                    r = request.Request('http://127.0.0.1:5000/user/{}'.format(au), method='POST', data=data)
+                    try:
+                        res = request.urlopen(r)
+                        print(res.read())
+                    except urllib.error.HTTPError as e:
+                        print(e.code, e.read())
+            else:
+                print('ONLY ADMIN CAN ADD USERS\n')
+        if(c == '3'):
+            if(isAdmin):
+                ap = input('Enter user_ID to edit details\t')
+                r0 = request.Request('http://127.0.0.1:5000/user/{}'.format(ap), method='GET')
+                try:
+                    r1 = request.urlopen(r0)
+                    print('Enter data to be updated\n')
+                    aup = {'user_ID' : '{}'.format(ap), 'password' : '{}'.format(input('Enter new user password\t')), 'role' : '{}'.format(input('Enter new user role\t')), 'isAdmin' : '{}'.format(input('Enter if new user is Admin or Not\t'))}
+                    data = urllib.parse.urlencode(aup)
+                    data = data.encode('ascii')
+                    r = request.Request('http://127.0.0.1:5000/user/{}'.format(ap), method='PUT', data=data)
+                    try:
+                        res = request.urlopen(r)
+                        print(res.read())
+                    except urllib.error.HTTPError as e:
+                        print(e.code, e.read())
+                except urllib.error.HTTPError as e:
+                    print('User {} doesn\'t exists'.format(ap))
+            else:
+                print('ONLY ADMIN CAN EDIT USER DETAILS\n')
+        if(c == '4'):
+            if(isAdmin):
+                ad = input('Enter user_ID to delete\t')
+                r = request.Request('http://127.0.0.1:5000/user/{}'.format(ad), method='DELETE')
+                try:
+                    res = request.urlopen(r)
+                    print(res.read())
+                except urllib.error.HTTPError as e:
+                    print(e.code, e.read())
+            else:
+                print('ONLY ADMIN CAN DELETE USERS\n')
         i = input('Press U for User Details or E to Exit\t')  
 
 def dist_func():
--- DistributorDetails.py
+++ DistributorDetails.py
@@ -4,7 +4,14 @@ from flask_restful import Api, Resource, reqparse, abort
 app = Flask(__name__)
 api = Api(app)
 
+parser = reqparse.RequestParser()
+
+parser.add_argument('password', type=str, help='Enter your password string')
+parser.add_argument('role', type=str, help='Enter your post')
+parser.add_argument('isAdmin', type=str, help="Enter Y or N", choices=['Y', 'N'])
+
 users = {'admin_head' : {'password' : 'abc', 'role' : 'IT_Admin', 'isAdmin' : 'Y'}}
+dis = {}
 
 class Login(Resource):
     def post(self, user_ID):
@@ -16,6 +23,33 @@ class Login(Resource):
         abort(404, message='User_ID {} or Password is invalid'.format(user_ID))
 
 api.add_resource(Login, '/login/<string:user_ID>')
+
+class User(Resource):
+    def get(self, user_ID=None):
+        if user_ID == None:
+            return users
+        if user_ID in users:
+            return users[user_ID]
+        abort(404, message='User {} doesn\'t exist'.format(user_ID))
+    def post(self, user_ID):
+        if user_ID not in users:
+            args = parser.parse_args()
+            users[user_ID] = args #{'password' : args['password'], 'role' : args['role'], 'isAdmin' : args['isAdmin']}
+            print(users)
+            return 'User_ID {} created'.format(user_ID)
+        abort(404, message='User {} already exists'.format(user_ID)) 
+    def put(self, user_ID):
+        if user_ID in users:
+            users[user_ID] = parser.parse_args()
+            return users[user_ID]
+        abort(404, message='User {} not in users'.format(user_ID))   
+    def delete(self, user_ID):
+        if user_ID in users:
+            del users[user_ID]
+            return "User {} deleted".format(user_ID)
+        abort(404, message='User {} doesn\'t exist'.format(user_ID))
+
+api.add_resource(User, '/user/<string:user_ID>', '/user')
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port='5000')
